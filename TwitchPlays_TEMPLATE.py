ff40cdf37717f4fdcd27c4b984c5c2655cf35705
Alex Boisvert <boisvert42@gmail.com>
2024-03-30 11:13:47 -0700

    gta -> crosswords

--- TwitchPlays_TEMPLATE.py
+++ TwitchPlays_TEMPLATE.py
@@ -9,14 +9,14 @@ from TwitchPlays_KeyCodes import *
 ##################### GAME VARIABLES #####################
 
 # Replace this with your Twitch username. Must be all lowercase.
-TWITCH_CHANNEL = 'dougdougw' 
+TWITCH_CHANNEL = 'boisvert42'
 
 # If streaming on Youtube, set this to False
 STREAMING_ON_TWITCH = True
 
 # If you're streaming on Youtube, replace this with your Youtube's Channel ID
 # Find this by clicking your Youtube profile pic -> Settings -> Advanced Settings
-YOUTUBE_CHANNEL_ID = "YOUTUBE_CHANNEL_ID_HERE" 
+YOUTUBE_CHANNEL_ID = "YOUTUBE_CHANNEL_ID_HERE"
 
 # If you're using an Unlisted stream to test on Youtube, replace "None" below with your stream's URL in quotes.
 # Otherwise you can leave this as "None"
@@ -26,16 +26,16 @@ YOUTUBE_STREAM_URL = None
 
 # MESSAGE_RATE controls how fast we process incoming Twitch Chat messages. It's the number of seconds it will take to handle all messages in the queue.
 # This is used because Twitch delivers messages in "batches", rather than one at a time. So we process the messages over MESSAGE_RATE duration, rather than processing the entire batch at once.
-# A smaller number means we go through the message queue faster, but we will run out of messages faster and activity might "stagnate" while waiting for a new batch. 
+# A smaller number means we go through the message queue faster, but we will run out of messages faster and activity might "stagnate" while waiting for a new batch.
 # A higher number means we go through the queue slower, and messages are more evenly spread out, but delay from the viewers' perspective is higher.
 # You can set this to 0 to disable the queue and handle all messages immediately. However, then the wait before another "batch" of messages is more noticeable.
 MESSAGE_RATE = 0.5
-# MAX_QUEUE_LENGTH limits the number of commands that will be processed in a given "batch" of messages. 
+# MAX_QUEUE_LENGTH limits the number of commands that will be processed in a given "batch" of messages.
 # e.g. if you get a batch of 50 messages, you can choose to only process the first 10 of them and ignore the others.
 # This is helpful for games where too many inputs at once can actually hinder the gameplay.
 # Setting to ~50 is good for total chaos, ~5-10 is good for 2D platformers
 MAX_QUEUE_LENGTH = 20
-MAX_WORKERS = 100 # Maximum number of threads you can process at a time 
+MAX_WORKERS = 3 # Maximum number of threads you can process at a time
 
 last_time = time.time()
 message_queue = []
@@ -44,6 +44,10 @@ active_tasks = []
 pyautogui.FAILSAFE = False
 
 ##########################################################
+
+# Letter mapping
+LETTER_MAPPING = {'a': A, 'b': B, 'c': C, 'd': D, 'e': E, 'f': F, 'g': G, 'h': H, 'i': I, 'j': J, 'k': K, 'l': L, 'm': M, 'n': N, 'o': O, 'p': P, 'q': Q, 'r': R, 's': S, 't': T, 'u': U, 'v': V, 'w': W, 'x': X, 'y': Y, 'z': Z}
+LETTER_SET = frozenset(LETTER_MAPPING.keys())
 
 # Count down before starting, so you have time to load up the game
 countdown = 5
@@ -72,52 +76,54 @@ def handle_message(message):
         # Use the "HoldAndReleaseKey(KEYCODE, SECONDS)" function press down a key for X seconds, then release it.
         # Use the pydirectinput library to press or move the mouse
 
-        # I've added some example videogame logic code below:
-
         ###################################
-        # Example GTA V Code 
+        # Crossword code
         ###################################
 
-        # If the chat message is "left", then hold down the A key for 2 seconds
-        if msg == "left": 
-            HoldAndReleaseKey(A, 2)
+        if msg == "left":
+            HoldAndReleaseKey(LEFT_ARROW, 0.1)
+        if msg == "right":
+            HoldAndReleaseKey(RIGHT_ARROW, 0.1)
+        if msg == "up":
+            HoldAndReleaseKey(UP_ARROW, 0.1)
+        if msg == "down":
+            HoldAndReleaseKey(DOWN_ARROW, 0.1)
 
-        # If the chat message is "right", then hold down the D key for 2 seconds
-        if msg == "right": 
-            HoldAndReleaseKey(D, 2)
+        if msg in LETTER_SET:
+            HoldAndReleaseKey(LETTER_MAPPING[msg], 0.1)
 
-        # If message is "drive", then permanently hold down the W key
-        if msg == "drive": 
-            ReleaseKey(S) #release brake key first
-            HoldKey(W) #start permanently driving
-
-        # If message is "reverse", then permanently hold down the S key
-        if msg == "reverse": 
-            ReleaseKey(W) #release drive key first
-            HoldKey(S) #start permanently reversing
-
-        # Release both the "drive" and "reverse" keys
-        if msg == "stop": 
-            ReleaseKey(W)
-            ReleaseKey(S)
-
-        # Press the spacebar for 0.7 seconds
-        if msg == "brake": 
-            HoldAndReleaseKey(SPACE, 0.7)
-
-        # Press the left mouse button down for 1 second, then release it
-        if msg == "shoot": 
-            pydirectinput.mouseDown(button="left")
-            time.sleep(1)
-            pydirectinput.mouseUp(button="left")
-
-        # Move the mouse up by 30 pixels
-        if msg == "aim up":
-            pydirectinput.moveRel(0, -30, relative=True)
-
-        # Move the mouse right by 200 pixels
-        if msg == "aim right":
-            pydirectinput.moveRel(200, 0, relative=True)
+        ## Legacy GTA stuff ##
+        # # If message is "drive", then permanently hold down the W key
+        # if msg == "drive":
+        #     ReleaseKey(S) #release brake key first
+        #     HoldKey(W) #start permanently driving
+        # # If message is "reverse", then permanently hold down the S key
+        # if msg == "reverse":
+        #     ReleaseKey(W) #release drive key first
+        #     HoldKey(S) #start permanently reversing
+        #
+        # # Release both the "drive" and "reverse" keys
+        # if msg == "stop":
+        #     ReleaseKey(W)
+        #     ReleaseKey(S)
+        #
+        # # Press the spacebar for 0.7 seconds
+        # if msg == "brake":
+        #     HoldAndReleaseKey(SPACE, 0.7)
+        #
+        # # Press the left mouse button down for 1 second, then release it
+        # if msg == "shoot":
+        #     pydirectinput.mouseDown(button="left")
+        #     time.sleep(1)
+        #     pydirectinput.mouseUp(button="left")
+        #
+        # # Move the mouse up by 30 pixels
+        # if msg == "aim up":
+        #     pydirectinput.moveRel(0, -30, relative=True)
+        #
+        # # Move the mouse right by 200 pixels
+        # if msg == "aim right":
+        #     pydirectinput.moveRel(200, 0, relative=True)
 
         ####################################
         ####################################
@@ -162,4 +168,3 @@ while True:
                 active_tasks.append(thread_pool.submit(handle_message, message))
             else:
                 print(f'WARNING: active tasks ({len(active_tasks)}) exceeds number of workers ({MAX_WORKERS}). ({len(message_queue)} messages in the queue)')
- 
